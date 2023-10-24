4b5e91fec84163777699f1f696116bcb10ad5aff
sakaorat36 <sakaoratploy@gmail.com>
2023-10-24 09:58:44 +0700

    login delete password

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -28,7 +28,7 @@ exports.register = async (req, res, next) => {
         expiresIn: process.env.JWT_EXPIRE,
       }
     );
-
+    delete user.password;
     res.status(201).json({ accessToken, user });
   } catch (error) {
     next(error);
@@ -38,9 +38,11 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const { value, error } = loginSchema.validate(req.body);
+
     if (error) {
       return next(error);
     }
+
     console.log(value);
     const user = await prisma.user.findUnique({
       where: {
@@ -51,17 +53,19 @@ exports.login = async (req, res, next) => {
     if (!user) {
       res.status(400).json({ message: "invalid credential" });
     }
-    const isMatch = await bcrypt.compare(value.password, user.password);
 
+    const isMatch = await bcrypt.compare(value.password, user.password);
     if (!isMatch) {
-      return next(createError("invalid credential", 400));
+      res.status(400).json({ message: "invalid credential" });
     }
+
     const payload = { userId: user.id };
     const accessToken = jwt.sign(
       payload,
       process.env.JWT_SECRET_KEY || "qwertyuiopasdfghjklzxcvbnm",
       { expiresIn: process.env.JWT_EXPIRE }
     );
+    delete user.password;
     res.status(200).json({ accessToken, user });
   } catch (error) {
     next(error);
